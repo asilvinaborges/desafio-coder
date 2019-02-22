11bc5e4105fc66bec3d430501b1b8693555ffc11
asilvinab <asilvinaborges@gmail.com>
2019-02-21 23:57:11 -0300

    Desafío Coder

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+$('header .menu').click(function(){
+    $('header nav').css({
+        top: 0, 
+    });
+});
+
+
+$('header .cerrar').click(function(){
+    $('header nav').css({
+        top: -2500, 
+    });
+});
